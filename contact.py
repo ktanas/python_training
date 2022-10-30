26b3d9a96c353b664d782d69d57584177acf77bd
ktanas <Krzysztof.Tanas@cs.put.poznan.pl>
2022-10-30 14:59:17 +0100

    Added tests for Contact class and adding a new contact to address book

---
+++ contact.py
@@ -0,0 +1,57 @@
+class Contact:
+
+    def __init__(self,
+                 firstname,
+                 middlename,
+                 lastname,
+                 nickname,
+                 photo_file_location,
+                 title,
+                 company,
+                 address,
+                 home_phone,
+                 mobile_phone,
+                 work_phone,
+                 fax,
+                 email,
+                 email2,
+                 email3,
+                 home_page,
+                 birth_day,
+                 birth_month,
+                 birth_year,
+                 anniversary_day,
+                 anniversary_month,
+                 anniversary_year,
+                 group_name,
+                 extra_address,
+                 extra_phone,
+                 notes
+                 ):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.photo_file_location = photo_file_location
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.home_page = home_page
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.group_name = group_name
+        self.extra_address = extra_address
+        self.extra_phone = extra_phone
+        self.notes = notes
+
