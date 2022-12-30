5e5865b7f0ea5f3ff87e69993566f2a17ed564e5
ktanas <Krzysztof.Tanas@cs.put.poznan.pl>
2022-12-30 10:13:11 +0100

    Added basic database operations

---
+++ fixture/db.py
@@ -0,0 +1,27 @@
+import pymysql
+from model.group import Group
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT group_id, group_name, group_header, group_footer FROM group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), group_name=name, header_name=header, footer_name=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
